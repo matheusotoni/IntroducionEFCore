8cb306a6fe104a74ea7eaba2f3eb25bee7bce118
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2023-08-11 12:27:32 -0300

    Create solution

---
+++ Course/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
